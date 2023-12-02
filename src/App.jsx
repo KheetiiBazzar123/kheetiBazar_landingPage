b8e2e9f438e2a739f497909661250d3cb790a0bd
nidhi <nidhi7485@gmail.com>
2023-12-02 22:39:58 +0530

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gif from './assets/images/hi-gif-2.gif'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={gif} alt='image' />
+      </div>
+    </>
+  )
+}
+
+export default App
