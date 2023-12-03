2b37dfca4d6b2b09d9440ab030c5063cf16767d7
nidhi <nidhi7485@gmail.com>
2023-12-03 22:40:40 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css';
-import Header from './pages/header/Header';
+
 import Main from './pages/main/Main';
 import Footer from './pages/footer/Footer';
 import About from './pages/about/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './pages/header/Header';
+
 function App() {
 	return (
 		<>
 			<Header />
-			<Main />
-			<Footer />
+			{/* <Main />
+			<Footer /> */}
 		</>
 	);
 }
--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,24 +1,23 @@
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { Row, Stack } from 'react-bootstrap/esm';
-
+import { Navbar, Nav, NavItem, Container, NavDropdown } from 'react-bootstrap';
+// import { NavItem, Row, Stack } from 'react-bootstrap/esm';
+import logo from '../../assets/images/logo.png';
 const Header = () => {
 	return (
 		<>
-			<Navbar bg="light" expand="lg">
+			<Navbar expand="lg" className="navbar-header">
 				<Container>
-					<Navbar.Brand href="#home">MyApp</Navbar.Brand>
+					<Navbar.Brand href="#/">
+						<img className="header-logo" src={logo} alt="logo" />
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
+					<Navbar.Collapse className="nav-collapse" id="basic-navbar-nav">
+						<Nav className=" nav-links">
 							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#link">Link</Nav.Link>
-							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-							</NavDropdown>
+							<Nav.Link href="#link">About Us</Nav.Link>
+							<Nav.Link href="#link">Services</Nav.Link>
+							<Nav.Link href="#link" className="contact-heading">
+								Contact Us
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
