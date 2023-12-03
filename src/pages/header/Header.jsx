69df618d52c1441230d30afd6c9c451025b71043
nidhi <nidhi7485@gmail.com>
2023-12-03 12:00:24 +0530

    added css variable and change tittle in index file and remove eslint config

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
-  return (
-    // TODO THis is react bootstrap navbar component if you think we don't need to use every component of react bootstrap then we can remove it
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">MyApp</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<>
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">MyApp</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
 };
 
 export default Header;
