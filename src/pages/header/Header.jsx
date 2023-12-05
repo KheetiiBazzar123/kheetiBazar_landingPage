91519858fe50da7b37863c9ee19aa8931c05ddfb
nidhi <nidhi7485@gmail.com>
2023-12-05 08:43:46 +0530

    add css in navbar

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -5,19 +5,21 @@ import logo from '../../assets/images/header/logo.png';
 const Header = () => {
 	return (
 		<header className="header">
-			<Navbar bg="dark" variant="dark" expand="lg">
+			<Navbar bg="dark" variant="dark" expand="lg" className="nav-top">
 				<Container>
 					<Navbar.Brand href="#home">
 						<img src={logo} alt="Kheeti Bazaar" className="logo" />
 						<span className="brand-name">KHEETI BAZAAR</span>
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="ml-auto">
+					<Navbar.Collapse className="nav-item-right" id="basic-navbar-nav">
+						<Nav className="ml-auto navbar-links">
 							<Nav.Link href="#home">Home</Nav.Link>
 							<Nav.Link href="#about">About Us</Nav.Link>
 							<Nav.Link href="#services">Services</Nav.Link>
-							<Nav.Link href="#contact">Contact Us</Nav.Link>
+							<Nav.Link href="#contact" className="contact-link">
+								Contact Us
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
