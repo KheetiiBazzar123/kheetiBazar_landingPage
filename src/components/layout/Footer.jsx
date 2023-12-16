5a2240c38f7772f20273a0372df00563ab57574d
AmitZorrro <amit.thakur@zorrroservices.com>
2023-12-16 16:22:58 +0530

    fixing footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,7 +7,7 @@ import styles from '../../styles/components/footer/footer.module.css';
 
 const Footer = () => {
 	return (
-		<div className={styles.footerContainer}>
+		<footer className={styles.footer}>
 			<Container>
 				<Row>
 					<Col className={styles.contactInfo}>
@@ -22,12 +22,63 @@ const Footer = () => {
 							<FontAwesomeIcon icon={faYoutube} className={styles.icon} /> */}
 						</div>
 					</Col>
-					<Col>
-						<div className={styles.map}>{/* Here you would embed your map */}</div>
+					<Col md={6} lg={3}>
+						<h4 className={styles.footerColTitle}>get help</h4>
+						<ul className={styles.footerColList}>
+							<li>
+								<a href="#">FAQ</a>
+							</li>
+							<li>
+								<a href="#">shipping</a>
+							</li>
+							<li>
+								<a href="#">returns</a>
+							</li>
+							<li>
+								<a href="#">order status</a>
+							</li>
+							<li>
+								<a href="#">payment options</a>
+							</li>
+						</ul>
+					</Col>
+					<Col md={6} lg={3}>
+						<h4 className={styles.footerColTitle}>online shop</h4>
+						<ul className={styles.footerColList}>
+							<li>
+								<a href="#">watch</a>
+							</li>
+							<li>
+								<a href="#">bag</a>
+							</li>
+							<li>
+								<a href="#">shoes</a>
+							</li>
+							<li>
+								<a href="#">dress</a>
+							</li>
+						</ul>
+					</Col>
+					<Col md={6} lg={3}>
+						<h4 className={styles.footerColTitle}>follow us</h4>
+						<div className={styles.socialLinks}>
+							<a href="#">
+								<FontAwesomeIcon icon={faFacebookF} />
+							</a>
+							<a href="#">
+								<FontAwesomeIcon icon={faTwitter} />
+							</a>
+							<a href="#">
+								<FontAwesomeIcon icon={faInstagram} />
+							</a>
+							<a href="#">
+								<FontAwesomeIcon icon={faLinkedinIn} />
+							</a>
+						</div>
 					</Col>
 				</Row>
 			</Container>
-		</div>
+		</footer>
 	);
 };
 
